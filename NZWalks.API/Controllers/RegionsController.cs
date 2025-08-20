e0c30e4c8f8bb46534cba58401f6c30e268188c5
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-08-20 21:38:34 +0600

    Get Regions by Id
    using Find method and FirstOrDefault method

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -17,12 +17,31 @@ namespace NZWalks.API.Controllers
             this.dbContext = dbContext;
         }
 
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
             var regions = dbContext.Regions.ToList();
 
             return Ok(regions);
+        }
+
+        // GET SINGLE REGION (Get region by ID)
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // var region = dbContext.Regions.Find(id);
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
         }
     }
 }
